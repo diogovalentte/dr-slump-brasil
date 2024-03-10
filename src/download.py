9b399b1343bd5f914166bc911fb19006b875ee0c
Diogo Valentim <diogovalentte10@gmail.com>
2024-03-10 10:25:53 -0300

    Also save renamed file when downloading from MEGAnz

--- src/download.py
+++ src/download.py
@@ -11,7 +11,12 @@ def download_from_url(
     download_url: str, download_folder: str, title: str, media_type: str
 ):
     if "mega.nz" in download_url:
-        download_mega(download_url, download_folder)
+        filename = get_filename_to_save(title, media_type)
+        if filename is None:
+            raise ValueError(
+                f"Media type: {media_type} is not supported while getting filename to save."
+            )
+        download_mega(download_url, download_folder, filename)
     elif "drive.google.com" in download_url:
         filename = get_filename_to_save(title, media_type)
         if filename is None:
@@ -30,10 +35,11 @@ def download_from_url(
         raise ValueError("The download_url is not supported.")
 
 
-def download_mega(download_url: str, download_folder: str):
+def download_mega(download_url: str, download_folder: str, filename: str):
     """Download the file from the download_url and save it to the download_folder."""
     mega = Mega()
-    mega.download_url(download_url, download_folder, ignore_quota_warn=True)
+    file_path = os.path.join(download_folder, filename)
+    mega.download_url(download_url, file_path, ignore_quota_warn=True)
 
 
 def download_gdrive(download_url: str, download_folder: str, filename: str):
